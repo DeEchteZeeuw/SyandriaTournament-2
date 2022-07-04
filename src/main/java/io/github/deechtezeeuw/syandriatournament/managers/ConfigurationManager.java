0a94ea3e178ee94a430f9204512659f7099ef184
DeEchteZeeuw <rubendalebout@gmail.com>
2022-07-04 20:12:01 +0200

    Added some ASCII to the config

--- src/main/java/io/github/deechtezeeuw/syandriatournament/managers/ConfigurationManager.java
+++ src/main/java/io/github/deechtezeeuw/syandriatournament/managers/ConfigurationManager.java
@@ -1,11 +1,18 @@
 package io.github.deechtezeeuw.syandriatournament.managers;
 
+import io.github.deechtezeeuw.syandriatournament.SyandriaTournament;
 import io.github.deechtezeeuw.syandriatournament.configurations.Tournaments;
 
+import java.io.File;
+
 public class ConfigurationManager {
-    private Tournaments tournaments = new Tournaments();
+    private Tournaments tournaments;
 
     public ConfigurationManager() {
+        // Check if configuration folder exists
+        File dataFolder = new File(SyandriaTournament.getInstance().getDataFolder()+"/configuration");
+        if (!dataFolder.exists()) dataFolder.mkdir();
+
         this.tournaments = new Tournaments();
     }
 
