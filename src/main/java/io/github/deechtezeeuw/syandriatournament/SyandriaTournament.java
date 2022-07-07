9120b5bf1bae3596f6631ef170433a76b947bc5f
DeEchteZeeuw <rubendalebout@gmail.com>
2022-07-07 19:42:47 +0200

    fix

--- src/main/java/io/github/deechtezeeuw/syandriatournament/SyandriaTournament.java
+++ src/main/java/io/github/deechtezeeuw/syandriatournament/SyandriaTournament.java
@@ -1,10 +1,14 @@
 package io.github.deechtezeeuw.syandriatournament;
 
 import io.github.deechtezeeuw.syandriatournament.managers.*;
+import io.github.deechtezeeuw.syandriatournament.models.Tournament;
 import io.github.deechtezeeuw.syandriatournament.utils.GUI;
 import io.github.deechtezeeuw.syandriatournament.utils.TextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.UUID;
 
 public final class SyandriaTournament extends JavaPlugin {
     private static SyandriaTournament instance;
@@ -52,6 +56,36 @@ public final class SyandriaTournament extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        // Give peoples inventory back
+        if (getTournamentManager().isActiveTournament()) {
+            for (UUID keys : getTournamentManager().getCurrentTournament().getLockerRoomManager().getInventory().keySet()) {
+                if (Bukkit.getServer().getPlayer(keys) != null) {
+                    Bukkit.getServer().getPlayer(keys).getInventory().clear();
+                    Bukkit.getServer().getPlayer(keys).getInventory().setContents(getTournamentManager().getCurrentTournament().getLockerRoomManager().getInventory(keys));
+                }
+            }
+
+            for (UUID keys : getTournamentManager().getCurrentTournament().getLockerRoomManager().getLocation().keySet()) {
+                if (Bukkit.getServer().getPlayer(keys) != null) {
+                    Bukkit.getServer().getPlayer(keys).teleport(getTournamentManager().getCurrentTournament().getLockerRoomManager().getLocation(keys));
+                }
+            }
+        }
+
+        for (Tournament tournament : getTournamentManager().getArrayRegistreredTournaments()) {
+            for (UUID keys : tournament.getLockerRoomManager().getInventory().keySet()) {
+                if (Bukkit.getServer().getPlayer(keys) != null) {
+                    Bukkit.getServer().getPlayer(keys).getInventory().clear();
+                    Bukkit.getServer().getPlayer(keys).getInventory().setContents(tournament.getLockerRoomManager().getInventory(keys));
+                }
+            }
+
+            for (UUID keys : tournament.getLockerRoomManager().getLocation().keySet()) {
+                if (Bukkit.getServer().getPlayer(keys) != null) {
+                    Bukkit.getServer().getPlayer(keys).teleport(tournament.getLockerRoomManager().getLocation(keys));
+                }
+            }
+        }
         // Plugin shutdown logic
         console.sendMessage(color.color("&7&m----------------------------------------"));
         console.sendMessage(color.color("&9&l" + getDescription().getName() + " &cdisabled!"));
