3ef62f1bdf6a0de7be4362b9b374344a90131e6a
DeEchteZeeuw <rubendalebout@gmail.com>
2022-07-07 19:22:04 +0200

    on death fix

--- src/main/java/io/github/deechtezeeuw/syandriatournament/listeners/OnDeath.java
+++ src/main/java/io/github/deechtezeeuw/syandriatournament/listeners/OnDeath.java
@@ -16,40 +16,62 @@ public class OnDeath implements Listener {
     public void onDeath(PlayerDeathEvent e) {
         Player player = e.getEntity();
 
-        if (plugin.getTournamentManager().isActiveTournament()) {
-            if (plugin.getTournamentManager().getCurrentTournament().isParticipant(player.getUniqueId())) {
-                if (plugin.getTournamentManager().getCurrentTournament().isInBattle(player.getUniqueId())) {
-                    if (plugin.getTournamentManager().getCurrentTournament().getTeams()) {
+        // Check if player is in current tournament playing
+        if (plugin.getTournamentManager().isActiveTournament() && plugin.getTournamentManager().getCurrentTournament().isParticipant(player.getUniqueId())) {
+            // Check if the player is in battle
+            if (plugin.getTournamentManager().getCurrentTournament().isInBattle(player.getUniqueId())) {
+                // Player is in battle check if the battle is teams
+                if (plugin.getTournamentManager().getCurrentTournament().getTeams()) {
+                    // Teams
+                    // Check if other teams win now
+                    ArrayList<UUID> teamOne = plugin.getTournamentManager().getCurrentTournament().getFightingTeamOne();
+                    ArrayList<UUID> teamTwo = plugin.getTournamentManager().getCurrentTournament().getFightingTeamTwo();
+                    ArrayList<UUID> teamThree = plugin.getTournamentManager().getCurrentTournament().getFightingTeamThree();
+                    ArrayList<UUID> teamFour = plugin.getTournamentManager().getCurrentTournament().getFightingTeamFour();
 
-                    } else {
-                        if (plugin.getTournamentManager().getCurrentTournament().battleSize() == 1) {
-                            ArrayList<UUID> winner = null;
-                            if (plugin.getTournamentManager().getCurrentTournament().getFightingTeamOne().size() == 1) winner = plugin.getTournamentManager().getCurrentTournament().getFightingTeamOne();
-                            if (plugin.getTournamentManager().getCurrentTournament().getFightingTeamTwo().size() == 1) winner = plugin.getTournamentManager().getCurrentTournament().getFightingTeamTwo();
-                            if (plugin.getTournamentManager().getCurrentTournament().getFightingTeamThree().size() == 1) winner = plugin.getTournamentManager().getCurrentTournament().getFightingTeamThree();
-                            if (plugin.getTournamentManager().getCurrentTournament().getFightingTeamFour().size() == 1) winner = plugin.getTournamentManager().getCurrentTournament().getFightingTeamFour();
+                    // Remove temp quiting player
+                    if (teamOne.contains(player.getUniqueId())) teamOne.remove(player.getUniqueId());
+                    if (teamTwo.contains(player.getUniqueId())) teamTwo.remove(player.getUniqueId());
+                    if (teamThree.contains(player.getUniqueId())) teamThree.remove(player.getUniqueId());
+                    if (teamFour.contains(player.getUniqueId())) teamFour.remove(player.getUniqueId());
 
-                            if (winner != null) {
-                                plugin.getTournamentManager().getCurrentTournament().setBattleWinner(winner);
-                            }
-                        } else if (plugin.getTournamentManager().getCurrentTournament().battleSize() == 0) {
-                            plugin.getTournamentManager().getCurrentTournament().setBattle();
-                        }
+                    // Check if team one wins
+                    if (teamOne.size() > 0 && teamTwo.size() == 0 && teamThree.size() == 0 && teamFour.size() == 0) {
+                        // Set player as winner
+                        plugin.getTournamentManager().getCurrentTournament().setBattleWinner(teamOne);
                     }
 
-                    if (plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().hasInventory(player.getUniqueId())) {
-                        player.getInventory().setContents(plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().getInventory(player.getUniqueId()));
-                        plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().removeInventory(player.getUniqueId());
+                    // Check if team two wins
+                    if (teamOne.size() == 0 && teamTwo.size() > 0 && teamThree.size() == 0 && teamFour.size() == 0) {
+                        // Set player as winner
+                        plugin.getTournamentManager().getCurrentTournament().setBattleWinner(teamTwo);
                     }
 
-                    if (plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().hasLocation(player.getUniqueId())) {
-                        player.teleport(plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().getLocation(player.getUniqueId()));
-                        plugin.getTournamentManager().getCurrentTournament().getLockerRoomManager().removeLocation(player.getUniqueId());
+                    // Check if team three wins
+                    if (teamOne.size() == 0 && teamTwo.size() == 0 && teamThree.size() > 0 && teamFour.size() == 0) {
+                        // Set player as winner
+                        plugin.getTournamentManager().getCurrentTournament().setBattleWinner(teamThree);
                     }
 
-                    plugin.getTournamentManager().getCurrentTournament().removePlayer(player.getUniqueId());
+                    // Check if team four wins
+                    if (teamOne.size() == 0 && teamTwo.size() == 0 && teamThree.size() == 0 && teamFour.size() > 0) {
+                        // Set player as winner
+                        plugin.getTournamentManager().getCurrentTournament().setBattleWinner(teamFour);
+                    }
+                } else {
+                    // Solo
+                    // Check if the battle contains 1 more player
+                    if (plugin.getTournamentManager().getCurrentTournament().battleSize() == 2) {
+                        ArrayList<UUID> winner = plugin.getTournamentManager().getCurrentTournament().getBattle();
+                        winner.remove(player.getUniqueId());
+
+                        // Set player as winner
+                        plugin.getTournamentManager().getCurrentTournament().setBattleWinner(winner);
+                    }
                 }
             }
+
+            plugin.getTournamentManager().getCurrentTournament().removePlayer(player.getUniqueId());
         }
     }
 }
